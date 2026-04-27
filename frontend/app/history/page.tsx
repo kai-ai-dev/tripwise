6f6336e1a7f3c41e9a5d13e097468314b671496c
Kai <shic68748@gmail.com>
2026-04-27 15:16:29 +0800

    Update page.tsx

--- frontend/app/history/page.tsx
+++ frontend/app/history/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import Navbar from '@/components/Navbar'
 
 const COLORS = [
   'from-blue-500 to-indigo-500',
